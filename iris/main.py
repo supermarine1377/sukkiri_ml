670b13d2e5af2396336cbbb635d9842b267ed73a
supermarine1377 <supermarine@outlook.jp>
2025-03-16 17:06:10 +0900

    initial commit

---
+++ iris/main.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+
+def main():
+    df = pd.read_csv("./iris.csv")
+    
+    xcols = [
+      "sepal_length", 
+      "sepal_width", 
+      "petal_length", 
+      "petal_width"
+    ]
+    ycols = [
+      "species"
+    ]
+    for xcol in xcols:
+      # print(col)
+      # print(df[col].mean())
+      # print(df[col].median())
+      df[xcol] = df[xcol].fillna(df[xcol].mean())
+    
+    xtrain, xtest, ytrain, ytest = train_test_split(df[xcols], df[ycols], test_size=0.3)
+    model = tree.DecisionTreeClassifier(max_depth=2)
+    model.fit(xtrain, ytrain)
+    print(model.score(xtest, ytest))
+    
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sukkiri-ml!")
+
+
+if __name__ == "__main__":
+    main()
